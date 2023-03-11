238e18d1f443c050ddf01f8bc55b752ad6f0cc28
lucaso-silva <luc.osilva@gmail.com>
2023-03-11 13:50:03 -0300

    feat: defined difficult, endpoint API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,34 @@
+import getImages from "./getImages.js";
 
-// const URL_API = 
+//variables
+const difficultModes = document.querySelectorAll("[name=difficult]");
+const startBtn = document.querySelector(".start");
+let numCards = 6;
+//functions
 
-console.log("hello world!")
+//Events
+difficultModes.forEach((mode) => {
+    mode.addEventListener("click", ()=>{
+        const difficultLevel = mode.id;
+
+        if(difficultLevel == "medium") {
+            numCards = 10;
+        }
+
+        if (difficultLevel == "hard") {
+            numCards = 16;
+
+        } 
+        
+        if (difficultLevel == "easy") {
+            numCards = 6;
+        }
+    })
+})
+
+startBtn.addEventListener("click", ()=>{
+    console.log(`btn Start clicado - jogo começará com ${numCards} cards`)
+})
+
+
+//getImages(5);
